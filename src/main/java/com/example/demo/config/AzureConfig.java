2c91c96b5c3908ced70cd6ab97ec09b193182682
loidinhm31 <loidinh.git@gmail.com>
2023-09-20 08:47:17 +0700

    Custom credential for ADF

--- src/main/java/com/example/demo/config/AzureConfig.java
+++ src/main/java/com/example/demo/config/AzureConfig.java
@@ -1,22 +1,37 @@
 package com.example.demo.config;
 
-import com.azure.core.credential.TokenCredential;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
-import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.resourcemanager.datafactory.DataFactoryManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AzureConfig {
+    @Value("${azure.clientId}")
+    private String clientId;
+
+    @Value("${azure.clientSecret}")
+    private String clientSecret;
+
+    @Value("${azure.tenantId}")
+    private String tenantId;
+
+    @Value("${azure.subscriptionId}")
+    private String subscriptionId;
+
     @Bean
     public DataFactoryManager DataFactoryManager() {
-        AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
-        TokenCredential credential = new DefaultAzureCredentialBuilder()
-                .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+        AzureProfile profile = new AzureProfile(tenantId, subscriptionId,AzureEnvironment.AZURE);
+        ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .tenantId(tenantId)
                 .build();
         return DataFactoryManager
-                .authenticate(credential, profile);
+                .authenticate(clientSecretCredential, profile);
     }
 }
